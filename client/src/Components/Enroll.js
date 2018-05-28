cf5903cefd06fe82b71b432384074f34c3cf29b8
adrianabelei <adrianabelei01@gmail.com>
2018-05-28 20:06:24 +0200

    coupon page

---
+++ client/src/Components/Enroll.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Redirect } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+export default class Enroll extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      data:{
+        coupon_id:''
+    
+      },
+      error:null,
+      errors:null,
+      isloggedIn:false
+    }
+    this.changeHandler = this.changeHandler.bind(this);
+    this.submitHandler = this.submitHandler.bind(this);
+  }
+  
+  submitHandler(e){
+    e.preventDefault();
+    axios
+    .post(process.env.REACT_APP_BACKEND + "/api/homelogin", this.state)
+    .then(response => {
+      this.props.history.push('/home')
+    })
+    .catch(err => {
+      if (err) {
+        this.setState({ error: err.response.data.errors });
+      }
+    });
+}
+  changeHandler(e){
+    var formData = this.state.data;
+    formData[e.target.name] = e.target.value;
+    this.setState({
+        data : formData
+    })
+
+  }
+    
+  render() {
+      var changeHandler= this.changeHandler;
+    return (
+      this.state.isloggedIn ? <Redirect to='/home' /> :
+      <div className='loginform'>
+        <h3>Coupon</h3>
+        {this.state.error && <p style={{color: 'red'}} >{this.state.error}</p> }
+      <form onSubmit={this.submitHandler}>
+            <div className="form-group">
+                        <label htmlFor="coupon_id">Coupon</label>
+                        <input type="coupon_id" value={this.state.data.coupon_id} name="coupon_id" onChange={changeHandler} className="form-control" id="coupon_id" aria-describedby="coupon_id" placeholder="Enter coupon"/>
+                        {this.state.errors && this.state.errors.coupon_id && <p className='text-danger' >{this.state.errors.coupon_id.msg}</p>  }
+
+            </div>
+            
+            <button type="submit" className="btn btn-primary">Enter</button>
+</form>
+      </div>
+    )
+  }
+}
