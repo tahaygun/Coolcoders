5240ea0e1772b0bfb3a6185b910e58a198eef60a
adrianabelei <adrianabelei01@gmail.com>
2018-05-30 21:53:33 +0200

    fixed add admin

--- client/src/Components/admin/AddAdmin.js
+++ client/src/Components/admin/AddAdmin.js
@@ -1,62 +1,82 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-export class AddAdmin extends Component {
-    constructor(props) {
-        super(props)
-      
-        this.state = {
-           username:'',
-           password:'',
-           error: ''
-        }
-        this.formHandler = this.formHandler.bind(this);
-        this.submitHandler = this.submitHandler.bind(this);
-    }
-      
-      formHandler(e){
-          this.setState({
-              [e.target.name]: e.target.value
-          })
-      }
-      submitHandler(e){
-          e.preventDefault();
-          axios
-          .post(process.env.REACT_APP_BACKEND + "/api/addadmin",this.state)
-          .then(res=>{
-            if (res.data.book) {
-              this.setState({
-                username:'',
-                password:'',
-                error: ''
-              });
-              setTimeout(()=>{ this.setState({message:null}); }, 3000);
-            }
-          })
-          .catch(err=>{
-            this.setState({error:'Unauthorized'})
-          });
-      }
-  render() {
-    return (
-      <div className='content-wrapper container' >
-      <h1>Add admin</h1><br/>
-        <p> {this.state.error}</p>
-        <p className='text-danger' >{this.state.message && this.state.message}</p>
-        <form onSubmit={this.submitHandler}>
-          
-            <label htmlFor="username">Username</label> <br/>
-            <input className="form-control" required value={this.state.username} autoComplete="off" type="text" name="username" onChange={this.formHandler} id="username"/>
-            <hr/>
-            <label htmlFor="username">Password</label> <br/>
-            <input className="form-control" required value={this.state.password} autoComplete="off" type="text" name="password" onChange={this.formHandler} id="password"/>
-            <hr/>
-            
-            <hr/>
-            <button className='btn btn-primary' type="submit">Add</button>
-        </form>
-      </div>
-    )
-  }
-}
+import React, { Component } from "react";
+import axios from "axios";
 
-export default AddAdmin
+export class AddCoupon extends Component {
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        data: {
+          username: "",
+          password: ""
+          
+        },
+        message: null,
+        error: ""
+      };
+    }
+  
+    formHandler = e => {
+      var formData = this.state.data;
+      formData[e.target.name] = e.target.value;
+      this.setState({
+        data: formData
+      });
+    };
+
+    submitHandler = e => {
+      e.preventDefault();   
+      axios
+        .post(process.env.REACT_APP_BACKEND + "/api/addadmin", this.state.data)
+        .then(res => {
+          if (res.status === 200) {
+            this.setState({
+              data: {
+                username: "",
+                password: ""
+                
+              },
+              message: "Coupon added successfully.",
+              error: ""
+            });
+          }
+        })
+        .catch(err => {
+          this.setState({ error: " The field is empty!" });
+        });
+    };
+    render() {
+      return (
+        <div className="content-wrapper text-center container">
+          <h3>Add Coupon</h3>
+          <br />
+          <p> {this.state.error}</p>
+          <p className="text-danger">
+            {this.state.message && this.state.message}
+          </p>
+          <form onSubmit={this.submitHandler}>
+            <label htmlFor="couponCode">Coupon Code</label> <br />
+            <input
+              style={{ width: "50vmax", margin: "auto" }}
+              className="form-control"
+              required
+              value={this.state.data.name}
+              autoComplete="off"
+              type="text"
+              name="couponCode"
+              onChange={this.formHandler}
+              id="couponCode"
+            />
+            <br/>            
+            <br />
+            <button className="btn btn-primary" type="submit">
+              Add
+            </button>
+          </form>
+        </div>
+      );
+    }
+  }
+  
+  export default AddCoupon;
+  
